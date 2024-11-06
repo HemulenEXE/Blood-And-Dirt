215a8b18b08c5ac6528a569eab6667e56b37839b
Игнатьев Михаил <depcoffmel@gmail.com>
2024-11-06 15:10:45 +0300

    Добавил дополнительные проверки в скрипт PlayerMotion.

--- Assets/Scripts/PlayerMotion.cs
+++ Assets/Scripts/PlayerMotion.cs
@@ -2,49 +2,35 @@
 using UnityEngine;
 
 /// <summary>
-/// Класс передвижения игрока посредством клавиш WASD и компьютерной мыши
+/// Класс передвижения игрока посредством клавиш WASD и компьютерной мыши.
 /// </summary>
 public class PlayerMotion : MonoBehaviour
 {
     /// <summary>
-    /// Промежуток времени
+    /// Промежуток времени.
     /// </summary>
-    private static float deltaTime;
+    private static float _deltaTime;
     /// <summary>
-    /// Главная камера
+    /// Главная камера.
     /// </summary>
-    private Camera mainCamera;
+    private Camera _mainCamera;
     /// <summary>
-    /// Скорость передвижения игрока пешком
+    /// Пешая скорость.
     /// </summary>
     [SerializeField] private float _speedWalk;
-    public float SpeedWalk
-    {
-        get => _speedWalk;
-        set
-        {
-            if (value < 0) throw new ArgumentOutOfRangeException("PlayerMotion: SpeedWalk < 0");
-            _speedWalk = value;
-        }
-    }
     /// <summary>
-    /// Скорость передвижения игрока при беге
+    /// Скорость бега.
     /// </summary>
     [SerializeField] private float _speedRun;
-    public float SpeedRun
-    {
-        get => _speedRun;
-        set
-        {
-            if (value < 0) throw new ArgumentOutOfRangeException("PlayerMotion: SpeedRun < 0");
-            _speedRun = value;
-        }
-    }
     private void Awake()
     {
-        //Настройка полей при запуске сцены
-        mainCamera = Camera.main;
-        deltaTime = Time.fixedDeltaTime;
+        //Настройка полей
+        _mainCamera = Camera.main;
+        _deltaTime = Time.fixedDeltaTime;
+
+        if (_mainCamera == null) throw new ArgumentNullException("PlayerMotion: _mainCamera is mull");
+        if (_speedRun < 0) throw new ArgumentOutOfRangeException("PlayerMotion: _speedRun < 0");
+        if (_speedWalk < 0) throw new ArgumentOutOfRangeException("PlayerMotion: _speedWalk < 0");
     }
     private void FixedUpdate()
     {
@@ -52,38 +38,40 @@ public class PlayerMotion : MonoBehaviour
         Rotate();
     }
     /// <summary>
-    /// Передвижение игрока посредством клавиш WASD
+    /// Передвижение игрока посредством клавиш WASD.
     /// </summary>
     private void Move()
     {
         //Текущая скорость игрока в зависимости от состояния нажатия клавиши LeftShift
-        float speedCurrent = Input.GetKey(KeyCode.LeftShift) ? SpeedRun : SpeedWalk;
+        float speedCurrent = Input.GetKey(KeyCode.LeftShift) ? _speedRun : _speedWalk;
+
         //Отслеживание нажатия клавиш
         if (Input.GetKey(KeyCode.A))
         {
-            this.transform.position += Vector3.left * speedCurrent * deltaTime;
+            this.transform.position += Vector3.left * speedCurrent * _deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            this.transform.position += Vector3.right * speedCurrent * deltaTime;
+            this.transform.position += Vector3.right * speedCurrent * _deltaTime;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            this.transform.position += Vector3.up * speedCurrent * deltaTime;
+            this.transform.position += Vector3.up * speedCurrent * _deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            this.transform.position += Vector3.down * speedCurrent * deltaTime;
+            this.transform.position += Vector3.down * speedCurrent * _deltaTime;
         }
     }
     /// <summary>
-    /// Поворот игрока за компьтерной мышью
+    /// Поворот игрока за компьтерной мышью.
     /// </summary>
     private void Rotate()
     {
         //Вычисление положения компьютерной мыши в мировом пространстве
-        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0;
+
         //Вычисление угла поворота игрока за мышью
         Vector3 direction = mousePosition - this.transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //Преобразование радиан в градусы - равен 360 / (2 * pi)
