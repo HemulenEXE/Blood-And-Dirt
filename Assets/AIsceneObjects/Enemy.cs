d443977f999a57b57333270a7a87a5ab2d8c2aa3
Евтухов Дмитрий <justarcane@vk.com>
2024-11-11 21:01:47 +0300

    добавлено снятие тревоги, заглушки для методов оружия и немного рандома в обнаружение

--- Assets/AIsceneObjects/Enemy.cs
+++ Assets/AIsceneObjects/Enemy.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class Enemy : MonoBehaviour
 {
-    public int minVisionDistance, maxVisionDistance;
-    public int VisionAngle; //чем больше значение, тем больше угол обзора
+    public float minVisionDistance, maxVisionDistance;
+    public float VisionAngle; //чем больше значение, тем больше угол обзора
     public float EnemySpeed;
+
+    bool IsDetect=false;
+    public string WeaponType;
     Transform PlayerT, EnemyT, VisionT;
     RaycastHit2D RayOBJ;
+
+
     
     
     void Start()
@@ -27,23 +33,62 @@ public class Enemy : MonoBehaviour
         {
             if (Vector3.Distance(EnemyT.position, PlayerT.position)<maxVisionDistance && Vector3.Distance(EnemyT.position, PlayerT.position)>Vector3.Distance(VisionT.position, PlayerT.position)+VisionAngle)
             {
-                Debug.Log("player big DETECTED!!!");
-                //SetBool "IsDetected" to True;
+                IsDetect=true;
+                Debug.Log("Игрок обнаружен зрением врага");
+                InvokeRepeating("Hunt", 0, 0.015f);
+                //
+                CancelInvoke("Seeker");
             }
-            else if (Vector3.Distance(EnemyT.position, PlayerT.position)<minVisionDistance)
+            else if ((Vector3.Distance(EnemyT.position, PlayerT.position)<minVisionDistance && Random.Range(0, 10)==4)  ||  (Vector3.Distance(EnemyT.position, PlayerT.position)<1.5f))
             {
-                Debug.Log("player small DETECTED!!!");
-                //SetBool "IsDetected" to True;
-                
+                IsDetect=true;
+                Debug.Log("Враг неожиданно обернулся...");
+                InvokeRepeating("Hunt", 0, 0.015f);
+                //
+                CancelInvoke("Seeker");
             }
         }
     }
 
 
-    void Attack()
+
+
+void Hunt()
     {
-        
+         if (Vector3.Distance(EnemyT.position, PlayerT.position)<maxVisionDistance  && IsDetect)
+        {
+            //EnemyT.position=Vector2.MoveTowards(EnemyT.position, PlayerT.position, EnemySpeed);
+            Invoke("Damaging", 0.9f);
+        }
+        else
+        {
+            IsDetect=false;
+            InvokeRepeating("Seeker", 0, 0.8f);
+            Debug.Log("преследование прекращено");
+            
+            CancelInvoke("Hunt");
+        }
     }
+
+
+
+
+    void Damaging()
+    {
+        if (WeaponType=="Knife"  && Vector3.Distance(VisionT.position, PlayerT.position)<0.8f && Vector3.Distance(EnemyT.position, PlayerT.position)<1.3f )
+        {
+            //InvokeRepeating("", 0.3f, 0.7f);   //вызываем метод атаки ножом
+            Debug.Log("Нанесен ближний урон");
+        }
+        else if (WeaponType=="AK")
+        {
+            //InvokeRepeating("", 0.3f, 0.2f);  //вызываем стрельбу из автомата
+            Debug.Log("Нанесен урон огнестрелом");
+        }
+    }
+
+    
+    
 
 
     void Modes()
@@ -53,9 +98,7 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Vector3.Distance(EnemyT.position, PlayerT.position)<maxVisionDistance && Vector3.Distance(EnemyT.position, PlayerT.position)>minVisionDistance)//Need add bool "IsDetected"
-        {
-            EnemyT.position=Vector2.MoveTowards(EnemyT.position, PlayerT.position, EnemySpeed);
-        }
+       //Debug.Log($"Vision->Player{Vector3.Distance(VisionT.position, PlayerT.position)}");
+       //Debug.Log($"Enemy->Player{Vector3.Distance(EnemyT.position, PlayerT.position)}");
     }
 }
