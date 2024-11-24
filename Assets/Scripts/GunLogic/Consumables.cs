622c1d3b40da9ad4b24df64d08a5e81a9d7a0b36
Мария Корчагина <kmd2oo619@gmail.com>
2024-11-24 15:42:23 +0300

    Изменена логика работы с расходниками

---
+++ Assets/Scripts/GunLogic/Consumables.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Хранит кол-во каждого расходника, а также управляет его отображением на канвасе. 
+/// Навешивается на элемент ConsumablesUI в канвасе
+/// </summary>
+public class Consumables : MonoBehaviour
+{
+    //Иконки каждого расходника на канвасе
+    private Transform _smokeGrenade;
+    private Transform _simpleGrenade;
+    private Transform _firstAidKit;
+    private Transform _bandage;
+
+    //Кол-во каждого расходника
+    private static int _smokeGrenadeCount = 0;
+    private static int _simpleGrenadeCount = 0;
+    private static int _firstAidKitCount = 0;
+    private static int _bandageCount = 0;
+
+    public static int smokeGrenadeCount
+    {   get { return _smokeGrenadeCount; }
+        set { if (value < 0 || value > 5)
+                throw new ArgumentOutOfRangeException("Invalid value!");
+            _smokeGrenadeCount = value; }
+    }
+    public static int simpleGrenadeCount
+    {
+        get { return _simpleGrenadeCount; }
+        set { if (value < 0 || value > 5)
+                throw new ArgumentOutOfRangeException("Invalid value!");
+            _simpleGrenadeCount = value; }
+    }
+    public static int firstAidKitCount
+    {
+        get { return _firstAidKitCount; }
+        set { if (value < 0 || value > 5)
+                 throw new ArgumentOutOfRangeException("Invalid value!");
+            _firstAidKitCount = value; }
+    }
+    public static int bandageCount
+    {
+        get { return _bandageCount; }
+        set { if (value < 0 || value > 5)
+                throw new ArgumentOutOfRangeException("Invalid value!");
+            _bandageCount = value; }
+    }
+    private void Awake()
+    {
+        //Инициализация полей
+        _smokeGrenade = transform.GetChild(0);
+        _simpleGrenade = transform.GetChild(1);
+        _firstAidKit = transform.GetChild(3);
+        _bandage = transform.GetChild(4);
+    }
+    private void Update()
+    {
+        _smokeGrenade.GetComponentInChildren<TextMeshProUGUI>().text = _smokeGrenadeCount.ToString();
+        _simpleGrenade.GetComponentInChildren<TextMeshProUGUI>().text = _simpleGrenadeCount.ToString();
+        _firstAidKit.GetComponentInChildren<TextMeshProUGUI>().text = _firstAidKitCount.ToString();
+        Debug.Log(_bandage);
+        Debug.Log(_bandage.GetComponentInChildren<TextMeshProUGUI>());
+        _bandage.GetComponentInChildren<TextMeshProUGUI>().text = _bandageCount.ToString();
+    }
+}
--- Assets/Scripts/GunLogic/ConsumablesVCO.cs
+++ Assets/Scripts/GunLogic/ConsumablesVCO.cs
@@ -4,33 +4,45 @@ using UnityEngine;
 namespace InteractiveObjects{
     /// <summary>
     /// Класс, реализующий "расходники".<br/>
-    /// Этот скрипт позволяет забирать расходники (аптечку, бинты, гранаты) со сцены и изменяет показатель их количества на канвасе.
+    /// Этот скрипт позволяет забирать расходники (аптечку, бинты, гранаты) со сцены и изменяет показатель их количества.
     /// </summary>
     public class ConsumablesVCO : ClickedObject
     {
-        /// <summary>
-        /// Объект, показывающий текущее количество расходника у игрока.
-        /// </summary>
-        private GameObject _count;
-        /// <summary>
-        /// Настройка и проверка полей.
-        /// </summary>
-        protected override void Awake()
-        {
-            base.Awake();
-            Transform panel = GameObject.FindGameObjectWithTag("Canvas")?.transform?.Find("ConsumablesUI");
-            Transform icon = panel.Find(this.tag); //Иконка расходника на канвасе.
-            _count = icon.GetChild(0).gameObject;
-        }
         /// <summary>
         /// Взаимодействие с расходником.
         /// </summary>
         public override void Interact()
         {
-            if (_count.GetComponent<TextMeshProUGUI>().text != "5")
+            switch (this.tag)
             {
-                Destroy(this.gameObject);
-                _count.GetComponent<TextMeshProUGUI>().text = (int.Parse(_count.GetComponent<TextMeshProUGUI>().text) + 1).ToString();
+                case "firstAidKit":
+                    if (Consumables.firstAidKitCount < 5)
+                    {
+                        Destroy(this.gameObject);
+                        Consumables.firstAidKitCount++;
+                    }             
+                    break;
+                case "simpleGraned":
+                    if (Consumables.simpleGrenadeCount < 5)
+                    { 
+                        Destroy(this.gameObject);
+                        Consumables.simpleGrenadeCount++; 
+                    } 
+                    break;
+                case "smokeGraned":
+                    if (Consumables.firstAidKitCount < 5)
+                    {
+                        Destroy(this.gameObject);
+                        Consumables.smokeGrenadeCount++;
+                    }
+                    break;
+                case "bandage":
+                    if (Consumables.bandageCount < 5)
+                    {
+                        Destroy(this.gameObject);
+                        Consumables.bandageCount++; 
+                    }
+                    break;
             }
         }
     }
