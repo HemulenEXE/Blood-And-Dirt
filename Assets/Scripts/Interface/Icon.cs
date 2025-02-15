1033e702a9c4a95cfaa7e02989916f5ae67fdd95
Мария Корчагина <kmd2oo619@gmail.com>
2025-02-15 12:17:58 +0300

    Добавленна часть значков. Написаны скрипт ля управления иконками и для перемещения, увеличения\уменьшения дерева

--- Assets/Scripts/Interface/Icon.cs
+++ Assets/Scripts/Interface/Icon.cs
@@ -1,11 +1,13 @@
 ﻿using TMPro;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class Icon : MonoBehaviour
 {
     [SerializeField]
-    private Transform panel;
+    private Transform window;
     [SerializeField]
     private Sprite inactive;
     [SerializeField]
@@ -16,20 +18,37 @@ public class Icon : MonoBehaviour
     private string discription;
     [SerializeField]
     private string techDiscription;
-
-    public Sprite InActive() { return inactive; }
-    public Sprite Active() { return active; }
+    [SerializeField]
+    private int price;
+    public GameObject[] Privioses;
+    private bool isActive;
+    public bool Active() { return isActive; }
+    //активирует выбранный навык
+    public void OpenSkill()
+    {
+        if (Privioses.Length == 0 || Privioses.Any(gm => gm.GetComponentInChildren<Icon>().Active()))
+        {
+            isActive = true;
+            this.GetComponent<Image>().sprite = active;
+            foreach (Image img in this.transform.parent.GetComponentsInChildren<Image>())
+                img.color = new Color(167, 255, 255, 255);
+        }
+    }
     private void Start()
     {
         this.GetComponent<Image>().sprite = inactive;
 
+        //Включает окно с описанием способности
         this.GetComponent<Button>().onClick.AddListener(() =>
         {
-            panel.gameObject.SetActive(true);
-            panel = panel.transform.GetChild(0);
+            window.gameObject.SetActive(true);
+            Transform panel = window.transform.GetChild(0);
             panel.GetChild(0).GetComponent<TextMeshProUGUI>().text = title;
             panel.GetChild(1).GetComponent<TextMeshProUGUI>().text = discription;
             panel.GetChild(2).GetComponent<TextMeshProUGUI>().text = techDiscription;
+            panel.GetChild(3).GetComponent<TextMeshProUGUI>().text = "Стоимость: " + price;
+            panel.GetChild(4).GetComponent<Button>().onClick.RemoveAllListeners();
+            panel.GetChild(4).GetComponent<Button>().onClick.AddListener(OpenSkill);
         });
     }
 
---
+++ Assets/Scripts/Interface/Zoom&Motion.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class ZoomAndMotion : MonoBehaviour
+{
+    [SerializeField]
+    private float maxZoom;
+    [SerializeField]
+    private float minZoom;
+    [SerializeField]
+    private float radius; //радиус, в котором можно перемещать дерево
+    private Vector2 startPoint; //Исходная позиция и масштаб
+    private Vector2 startScale;
+    private Vector2 pos; 
+    private Vector2 offset; 
+    private Vector2 mousePos;
+    public Vector2 GStartPoint() {  return startPoint; }
+    public Vector2 StartScale() { return startScale; }
+    private void Start()
+    {
+        startScale = this.transform.localScale;
+        startPoint = this.transform.position;
+    }
+    void Update()
+    {
+        //Фиксация расстояния, на котором друг от друга должны находиться курсор мыши и дерево
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {   
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            pos = this.transform.position;
+            offset = mousePos - pos;
+        } //Перемещение дерева по траектории курсора
+        else if (Input.GetKey(KeyCode.Mouse1))
+        {
+            float x = this.transform.position.x, y = this.transform.position.y;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            if ((Mathf.Abs(mousePos.y - offset.y - startPoint.y) <= radius))
+                y = mousePos.y - offset.y; 
+            if ((Mathf.Abs(mousePos.x - offset.x - startPoint.x) <= radius))
+                x = mousePos.x - offset.x;
+            this.transform.position = new Vector2(x, y);
+            pos = this.transform.position;
+        }
+        //увелечение\уменьшение дерева
+        if ( this.transform.localScale.x -Input.GetAxis("Mouse ScrollWheel") >= minZoom && this.transform.localScale.x - Input.GetAxis("Mouse ScrollWheel") <= maxZoom)
+            this.transform.localScale += new Vector3(-Input.GetAxis("Mouse ScrollWheel"), -Input.GetAxis("Mouse ScrollWheel"));
+    }
+}
