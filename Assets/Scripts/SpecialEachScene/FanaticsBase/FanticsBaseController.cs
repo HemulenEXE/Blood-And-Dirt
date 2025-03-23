b0328c209aa53fccaeb1eae4f352e73cf9217ae9
Мария Корчагина <kmd2oo619@gmail.com>
2025-03-23 13:33:07 +0300

    исправленны ошибки с тайлсетом

---
+++ Assets/Scripts/SpecialEachScene/FanaticsBase/FanticsBaseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Отвечает за активацию/деактивацию нужных скриптов при каждом новом попадании на эту сцену
+public class FanticsBaseController : MonoBehaviour
+{
+    public int LoadingNumber; //Фиксирует число заходов на эту сцену, т.к. каждый раз на ней происходят разные действия
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
