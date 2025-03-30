991907bebf99b78d5ee19ae533d287b8070f22f6
Мария Корчагина <kmd2oo619@gmail.com>
2025-03-30 04:44:39 +0300

    Сделана первая кат-сцена в сцене 2 средней линии

---
+++ Assets/Scripts/SpecialEachScene/Scene2.1/PlayCatScene1_2_1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+//Запускает стартовую кат-сцену после диалога с Гектором в сцене 2.1 средней линии. Навешивается на Гектора
+public class PlayCatScene1_2_1 : MonoBehaviour
+{
+    [SerializeField] private TextAsset FileName;
+    private GameObject DialogueWindow;
+    private Dialogue _dialogue;
+    private ShowDialogueDubl _director;
+    private bool isTriger = false;
+    private void Start()
+    {
+        _director = GetComponent<ShowDialogueDubl>();
+        this.GetComponent<ShowDialogueDubl>().StartDialogue();
+        GameObject.FindWithTag("Player").GetComponent<PlayerMotion>().enabled = false;
+    }
+    void Update()
+    {
+        if (_director.FileName == FileName)
+        {
+            DialogueWindow = _director.DialogueWindow.gameObject;
+            _dialogue = _director.GetDialogue();
+        }
+
+        if (!isTriger && !DialogueWindow.activeSelf && (_dialogue.GetCurentNode().exit == "True"))
+        {
+            isTriger = true;
+            GameObject.Find("CatScene1").GetComponent<PlayableDirector>().Play();
+        }
+    }
+    //Активирует возможность двигаться после катсцены
+    public void ActivMotion()
+    {
+        GameObject.FindWithTag("Player").GetComponent<PlayerMotion>().enabled = false;
+    }
+}
