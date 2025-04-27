1bb3c610524fbe9d22452abc01cb1539d5013767
Евтухов Дмитрий <justarcane@vk.com>
2025-04-28 00:42:24 +0300

    добавлен метод активации катсцены 2

--- Assets/Scripts/SpecialEachScene/Scene3_1Fanatic/DialogueController3_1.cs
+++ Assets/Scripts/SpecialEachScene/Scene3_1Fanatic/DialogueController3_1.cs
@@ -11,11 +11,17 @@ public class DialogueController3_1 : MonoBehaviour
     private GameObject DialogueWindow;
     private Dialogue _dialogue;
     private ShowDialogueDubl _director;
+    private bool flag = false;
+
+    [SerializeField] GameObject CutScene2, MainCamera, CutsceneCamera;
+
     
 
     private void Start()
     {
+         flag = true;
         _director = GetComponent<ShowDialogueDubl>();
+        InvokeRepeating("FixedUpdateEvery1Sec", 0f, 1f);
     }
 
 
@@ -23,4 +29,21 @@ public class DialogueController3_1 : MonoBehaviour
     {
         _director.StartDialogue();
     }
+
+    void FixedUpdateEvery1Sec()
+    {
+        if (flag && !DialogueWindow.activeSelf && _dialogue.GetCurentNode().exit == "True") 
+        {
+            StartCutscene2();
+            CancelInvoke("FixedUpdateEvery1Sec");
+        }
+    }
+
+    void StartCutscene2()
+    {
+        CutScene2.SetActive(true);  
+        MainCamera.SetActive(true);
+        CutsceneCamera.SetActive(false); 
+    }
+
 }
