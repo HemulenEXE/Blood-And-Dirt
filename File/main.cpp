6a21d99f72bba34a03d935e996b752c24a134565
MID4 <146509640+CyanCactus4@users.noreply.github.com>
2025-04-28 15:51:37 +0300

    Add files via upload

---
+++ File/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "matrix.h"
+#include "entity.h"
+#include <vector>
+#include <list>
+
+using namespace IMD;
+
+int main() {
+	std::cout << FLT_MIN;
+
+	int value{ 2 };
+	int* const ptr = &value;
+	auto ptr1 = ptr + 2;
+
+}
