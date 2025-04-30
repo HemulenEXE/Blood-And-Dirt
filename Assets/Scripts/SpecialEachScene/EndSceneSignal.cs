5dce3f4ebe0110de05b65da20621523846405972
Евтухов Дмитрий <justarcane@vk.com>
2025-04-30 13:02:12 +0300

    Добавил затухающий экран в сцене 2.3

--- Assets/Scripts/SpecialEachScene/EndSceneSignal.cs
+++ Assets/Scripts/SpecialEachScene/EndSceneSignal.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndSceneSignal : MonoBehaviour
 {
+    public Image Frontground;
+
     public void EndScene()
     {
         ScenesManager.Instance.OnNextScene();
+    }
+
+    public void FadeToBlackFrontground()
+    {
+        InvokeRepeating("FadeToBlackFrontgroundCycle", 0, 0.02f);
+    }
+
+    void FadeToBlackFrontgroundCycle()
+    {
+        Frontground.color += new Color(0f, 0f, 0f, 0.002f);
+        if (Frontground.color.a >= 1f)
+        {
+            CancelInvoke("FadeToBlackFrontgroundCycle");
+        }
     }
 }
