48c6e831fe616c68eba1d760a864773138d71630
Игнатьев Михаил <depcoffmel@gmail.com>
2025-05-02 00:50:38 +0300

    Исправил баг с зависанием анимации игрока

--- Assets/Scripts/Weapons/ShrapnelBullet.cs
+++ Assets/Scripts/Weapons/ShrapnelBullet.cs
@@ -20,19 +20,25 @@ public class ShrapnelBullet : MonoBehaviour, IBullet
         Destroy(this.gameObject, _lifeTime);
         _previousPosition = transform.position;
     }
-    protected void OnCollisionEnter2D(Collision2D other)
+    private void FixedUpdate()
     {
-        if (!other.gameObject.CompareTag("Projectile") && !other.gameObject.CompareTag("Gun"))
+        Vector3 newPosition = transform.position + transform.right * Speed * Time.fixedDeltaTime;
+
+        // Проверяем наличие препятствий
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, Speed * Time.fixedDeltaTime);
+        if (hit.collider != null && !hit.collider.gameObject.CompareTag("Projectile") && !hit.collider.gameObject.CompareTag("Gun"))
         {
             Debug.Log("DESTROYED");
             Destroy(this.gameObject);
         }
-    }
-    private void FixedUpdate()
-    {
-        this.transform.position += this.transform.right * Speed * Time.fixedDeltaTime;
+        else
+        {
+            transform.position = newPosition;
+        }
 
         Debug.DrawLine(_previousPosition, transform.position, Color.red);
+
+        Debug.Log(hit.collider?.gameObject);
 
     }
 }
